47a3aad495a6f8e71a571bae0f1ce9b86cb84229
haidyelnahass <haidyelnahass@hotmail.com>
2022-05-29 23:04:18 +0200

    csp class functions

--- csp.py
+++ csp.py
@@ -1,3 +1,71 @@
+from typing import Callable, List, Optional, Tuple, Dict
+from utilities import argmin_random_tie, count, first
+import problem
+
+class CSP(problem.Problem):
+    def goal_test(self, state):
+        """
+        params:
+            state: a tuple of (var, val) pairs
+        """
+        # check if all variables are assigned
+        assignment = dict(state)
+        condition1 = len(assignment) == len(self.variables)
+        condition2 = all(self.nconflicts(variables, assignment[variables], assignment) == 0 for variables in self.variables)
+        return (condition1
+                and condition2)
+
+    # These are for constraint propagation
+
+    def support_pruning(self):
+        # Make sure we can prune values from domains.
+        if self.curr_domains is None:
+            self.curr_domains = {v: list(self.domains[v]) for v in self.variables}
+
+    def prune(self, var, value, removals):
+        """Rule out var=value."""
+        """
+        params
+            var: a variable
+            value: a value
+            removals: a list of (var, val) pairs"""
+        self.curr_domains[var].remove(value)
+        if removals is not None:
+            removals.append((var, value))
+    def assignVal(self, var, value):
+        # Start accumulating inferences from assuming var=value
+        """
+        params: var: a variable
+                value: a value
+        """
+        # remove value from cage domain
+        self.support_pruning()
+        removals = [(var, a) for a in self.curr_domains[var] if a != value]
+        # assign value to cell
+        self.curr_domains[var] = [value]
+        return removals
+
+    def infer_assignment(self):
+        """Return the partial assignment implied by the current inferences."""
+        self.support_pruning()
+        return {v: self.curr_domains[v][0]
+                for v in self.variables if self.curr_domains[v].__len__() ==1 }
+
+    def choices(self, var):
+        """Return all values for var that aren't currently ruled out."""
+        return (self.curr_domains or self.domains)[var]
+
+
+    def conflicted_vars(self, current):
+        # Return a list of variables in current assignment that are in conflict
+        return [var for var in self.variables
+                if self.nconflicts(var, current[var], current) > 0]
+    def restore(self, removals):
+        """Undo a supposition and all inferences from it."""
+        for B, b in removals:
+            self.curr_domains[B].append(b)
+
+    # This is for min_conflicts search
 
 
 
@@ -44,3 +112,4 @@ def removeInconsistentValues(csp_var, Xi, Xj, removals):
     return removedVal
 
 # -------------------------------------------------------------------------------------
+
