ebb4ab181ea09b2dc05b2af49e6ecdb584ca97c0
haidyelnahass <haidyelnahass@hotmail.com>
2022-05-29 22:33:40 +0200

    initialize utility functions for the game

---
+++ utilities.py
@@ -0,0 +1,51 @@
+import random
+
+
+
+
+
+
+
+def count(seq):
+    """Count the number of items in sequence that are interpreted as true."""
+    return sum(bool(x) for x in seq)
+
+
+def first(iterable, default=None):
+    """Return the first element of an iterable or the next element of a generator; or default."""
+    try:
+        return iterable[0]
+    except IndexError:
+        return default
+    except TypeError:
+        return next(iterable, default)
+
+
+def is_in(elt, seq):
+    """Similar to (elt in seq), but compares with 'is', not '=='."""
+    return any(x is elt for x in seq)
+
+
+
+# ______________________________________________________________________________
+# argmin and argmax
+
+
+identity = lambda x: x
+
+argmin = min
+
+
+def argmin_random_tie(seq, key=identity):
+    """Return a minimum element of seq; break ties at random."""
+    return argmin(shuffled(seq), key=key)
+
+
+
+def shuffled(iterable):
+    """Randomly shuffle a copy of iterable."""
+    items = list(iterable)
+    random.shuffle(items)
+    return items
+
+
